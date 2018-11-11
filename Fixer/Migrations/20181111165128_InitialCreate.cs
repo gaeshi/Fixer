c6f45c2707ddc05d280b1522e48162fc2fd064a1
Victor on Win10 <gaeshi@gmail.com>
2018-11-12 02:24:48 +0900

    Add Migrations for MovieDbContext

---
+++ Fixer/Migrations/20181111165128_InitialCreate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fixer.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false),
+                    VoteCount = table.Column<long>(nullable: false),
+                    VoteAverage = table.Column<double>(nullable: false),
+                    Title = table.Column<string>(nullable: true),
+                    Popularity = table.Column<double>(nullable: false),
+                    PosterPath = table.Column<string>(nullable: true),
+                    OriginalLanguage = table.Column<string>(nullable: true),
+                    OriginalTitle = table.Column<string>(nullable: true),
+                    BackdropPath = table.Column<string>(nullable: true),
+                    Adult = table.Column<bool>(nullable: false),
+                    Overview = table.Column<string>(nullable: true),
+                    ReleaseDate = table.Column<DateTime>(nullable: false),
+                    LastUpdated = table.Column<DateTime>(nullable: false),
+                    Filters = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
